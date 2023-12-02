89bc420d5d6a7656706a25b907caeee936403c8f
Jesper Hodge <jhodge@outlook.de>
2023-12-01 19:53:51 -0500

    feat: day 1 part 2 solved

--- src/day1.py
+++ src/day1.py
@@ -1,23 +1,50 @@
+import re
+
+numbers_regex = r'(?=(one|two|three|four|five|six|seven|eight|nine|0|1|2|3|4|5|6|7|8|9))'
+
+
+char_to_numeric = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
+
+
 def get_input_lines():
     with open('day1_input.txt') as f:
         lines = f.readlines()
     return lines
 
 
-def get_first_numeric(line):
-    for char in line:
-        if char.isnumeric():
-            return char
-    return ''
+def get_string_numbers_value(line):
+    value = ''
+    matches = re.findall(numbers_regex, line)
+    first = matches[0] if len(matches) > 0 else ''
+    last = matches[-1] if len(matches) > 0 else ''
+
+    if not first.isnumeric():
+        value += char_to_numeric[first]
+    else:
+        value += first
+    if not last.isnumeric():
+        value += char_to_numeric[last]
+    else:
+        value += last
+
+    return int(value) if value != '' else 0
+
+
 
 def calculate_line_value(line):
-    numeric = ''
-    numeric += get_first_numeric(line)
-    
-    reversed_line = line[::-1]
-    numeric += get_first_numeric(reversed_line)
-
-    return int(numeric)
+    number = get_string_numbers_value(line)
+    print(number)
+    return number
 
 
 def calculateCalibrationValue(lines):
@@ -29,7 +56,10 @@ def calculateCalibrationValue(lines):
 
 def main():
     lines = get_input_lines()
-    print(calculateCalibrationValue(lines))
+    res = calculateCalibrationValue(lines)
+    print(res)
+    return res
+
 
 
 if __name__ == "__main__":
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,9 +1,18 @@
 from src.day1 import main
 
+'''
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+'''
 def mock_get_input_lines():
-    return ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']
+    return ['two1nine', 'eightwothree', 'abcone2threexyz', 'xtwone3four', '4nineeightseven2', 'zoneight234', '7pqrstsixteen']
 
 def test_main(monkeypatch):
     monkeypatch.setattr('src.day1.get_input_lines', mock_get_input_lines)
     
-    assert main() == 142
+    assert main() == 281
